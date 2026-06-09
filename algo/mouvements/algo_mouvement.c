d66dbf75e26ec31f018a85960bcef9910ba93011
gerramir <gerramir@f4r7s6.paris.42.school>
2026-06-09 21:50:35 +0200

    data->ops-ss++

--- algo/mouvements/algo_mouvement.c
+++ algo/mouvements/algo_mouvement.c
@@ -6,7 +6,7 @@
 /*   By: gerramir <gerramir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/21 17:41:52 by axweinma          #+#    #+#             */
-/*   Updated: 2026/06/09 21:18:37 by gerramir         ###   ########.fr       */
+/*   Updated: 2026/06/09 21:47:53 by gerramir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,10 @@
 //         write(1, "sa\n", 3);
 // }
 
-void	sa(t_list **a, int print)
+void	sa(t_list **a, int print, t_data *data)
 {
 	t_list	*first;
 	t_list	*second;
-	t_ops	*ops;
 
 	if (!a || !*a || !(*a)->next)
 		return ;
@@ -43,18 +42,17 @@ void	sa(t_list **a, int print)
 	*a = second;
 	if (print)
 		write(1, "sa\n", 3);
-	ops->sa++;
-	ops->total++;
+	data->ops->sa++;
+	data->ops->total++;
 }
 
 /// sb (swap b): Swap the first two elements at the top of stack b.
 // Do nothing if there is only one or no elements.
 
-void	sb(t_list **b, int print) // on lui envoie ladresse de la a pour pouvoir modifier directement la stack
+void	sb(t_list **b, int print, t_data *data) // on lui envoie ladresse de la a pour pouvoir modifier directement la stack
 {
 	t_list	*first;
 	t_list	*second;
-	t_ops	*ops;
 
 	if (!b || !*b || !(*b)->next)
 		return ;
@@ -65,26 +63,27 @@ void	sb(t_list **b, int print) // on lui envoie ladresse de la a pour pouvoir mo
 	*b = second;
 	if (print)
 		write(1, "sb\n", 3);
-	ops->sb++;
-	ops->total++;
+	data->ops->sb++;
+	data->ops->total++;
 }
 
 /// ss (): sa and sb at the same time.
-void	ss(t_list **a, t_list **b)
+void	ss(t_list **a, t_list **b, t_data *data)
 {
 	sa(a, 0);
 	sb(b, 0);
 	write(1, "ss", 2);
+	data->ops->ss++;
+	data->ops->total++;
 }
 
 /// pa (push a): Take the first element at the top of b and put it at the top of a.
 // Do nothing if b is empty.
 
-void	pa(t_list **a, t_list **b)
+void	pa(t_list **a, t_list **b, t_data *data)
 {
 	t_list	*tmp1;
 	t_list	*tmp2;
-	t_ops	*ops;
 
 	if (!*b)
 		return ;
@@ -94,17 +93,16 @@ void	pa(t_list **a, t_list **b)
 	(*a)->next = tmp2;
 	*b = tmp1->next;
 	write(1, "pa", 2);
-	ops->pa++;
-	ops->total++;
+	data->ops->pa++;
+	data->ops->total++;
 }
 
 /// pb (push b): Take the first element at the top of a and put it at the top of b.
 // Do nothing if a is empty.
-void	pb(t_list **b, t_list **a)
+void	pb(t_list **b, t_list **a, t_data *data)
 {
 	t_list	*tmp1;
 	t_list	*tmp2;
-	t_ops	*ops;
 
 	if (!*a)
 		return ;
@@ -114,18 +112,17 @@ void	pb(t_list **b, t_list **a)
 	(*b)->next = tmp2;
 	*a = tmp1->next;
 	write(1, "pb", 2);
-	ops->pb++;
-	ops->total++;
+	data->ops->pb++;
+	data->ops->total++;
 }
 
 /// ra (rotate a): Shift up all elements of stack a by one.
 // The first element becomes the last one.
 
-void	ra(t_list **a, int print)
+void	ra(t_list **a, int print, t_data *data)
 {
 	t_list	*tmp;
 	t_list	*last;
-	t_ops	*ops;
 
 	if (!a || !*a || !(*a)->next)
 		return ;
@@ -138,23 +135,22 @@ void	ra(t_list **a, int print)
 	tmp->next = NULL;
 	if (print)
 		write(1, "ra\n", 3);
-	ops->ra++;
-	ops->total++;
+	data->ops->ra++;
+	data->ops->total++;
 }
 
 /// rb (rotate b): Shift up all elements of stack b by one.
 // The first element becomes the last one.
 
-void	rb(t_list **b, int print)
+void	rb(t_list **b, int print, t_data *data)
 {
 	t_list	*tmp;
-	t_list	lbst;
-	t_ops	*ops;
+	t_list	*lbst;
 
-	if (!b || !b || !(b)->next)
+	if (!b || !*b || !(*b)->next)
 		return ;
-	tmp = b;
-	b = (b)->next;
+	tmp = *b;
+	*b = (*b)->next;
 	lbst = *b;
 	while (lbst->next)
 		lbst = lbst->next;
@@ -162,30 +158,27 @@ void	rb(t_list **b, int print)
 	tmp->next = NULL;
 	if (print)
 		write(1, "rb\n", 3);
-	ops->rb++;
-	ops->total++;
+	data->ops->rb++;
+	data->ops->total++;
 }
 
 /// rr (): ra and rb at the same time.
-void	rr(t_list **a, t_list **b)
+void	rr(t_list **a, t_list **b, t_data *data)
 {
-	t_ops	*ops;
-
 	ra(a, 0);
 	rb(b, 0);
 	write(1, "rr", 2);
-	ops->rr++;
-	ops->total++;
+	data->ops->rr++;
+	data->ops->total++;
 }
 
 /// rra (reverse rotate a): Shift down all elements of stack a by one.
 /// The last element becomes the first one.
-void	rra(t_list **a, int print)
+void	rra(t_list **a, int print, t_data *data)
 {
 	t_list	*tmp1;
 	t_list	*tmp2;
 	t_list	*tmp3;
-	t_ops	*ops;
 
 	if (!a)
 		return ;
@@ -199,18 +192,17 @@ void	rra(t_list **a, int print)
 	*a = tmp3;
 	if (print)
 		write(1, "rra", 3);
-	ops->rra++;
-	ops->total++;
+	data->ops->rra++;
+	data->ops->total++;
 }
 
 /// rrb (reverse rotate b): Shift down all elements of stack b by one.
 // The last element becomes the first one.
-void	rrb(t_list **b, int print)
+void	rrb(t_list **b, int print, t_data *data)
 {
 	t_list	*tmp1;
 	t_list	*tmp2;
 	t_list	*tmp3;
-	t_ops	*ops;
 
 	if (!b)
 		return ;
@@ -224,18 +216,16 @@ void	rrb(t_list **b, int print)
 	*b = tmp3;
 	if (print)
 		write(1, "rrb", 3);
-	ops->rrb++;
-	ops->total++;
+	data->ops->rrb++;
+	data->ops->total++;
 }
 
 /// rrr (): rra and rrb at the same time
-void	rrr(t_list **a, t_list **b)
+void	rrr(t_list **a, t_list **b, t_data *data)
 {
-	t_ops	*ops;
-
 	rra(a, 0);
 	rrb(b, 0);
 	write(1, "rrr", 2);
-	ops->rrr++;
-	ops->total++;
+	data->ops->rrr++;
+	data->ops->total++;
 }
--- push_swap.h
+++ push_swap.h
@@ -31,7 +31,6 @@ typedef struct s_ops
 	int				total;
 }					t_ops;
 
-
 typedef struct s_data
 {
 	t_list			*a;
@@ -70,14 +69,16 @@ int					ft_findc(char *str, char c);
 t_list				*fonction_simple(t_list **a);
 int					valid_sort(t_list **a);
 /////// mouvements
-void				sa(t_list **a, int print);
-void				sb(t_list **b, int print);
-void				ss(t_list **a, t_list **b);
-void				ra(t_list **a, int print);
-void				rb(t_list **b, int print);
-void				rr(t_list **a, t_list **b);
-void				rra(t_list **a, int print);
-void				rrb(t_list **b, int print);
-void				rrr(t_list **a, t_list **b);
+void				sa(t_list **a, int print, t_data *data);
+void				sb(t_list **b, int print, t_data *data);
+void				ss(t_list **a, t_list **b, t_data *data);
+void				pa(t_list **a, t_list **b, t_data *data);
+void				pb(t_list **b, t_list **a, t_data *data);
+void				ra(t_list **a, int print, t_data *data);
+void				rb(t_list **b, int print, t_data *data);
+void				rr(t_list **a, t_list **b, t_data *data);
+void				rra(t_list **a, int print, t_data *data);
+void				rrb(t_list **b, int print, t_data *data);
+void				rrr(t_list **a, t_list **b, t_data *data);
 
 #endif
